9aa19c452f3aa09053bafacbde8c936d38c1945f
Lucas Zubeldia <lzubeldia@gmail.com>
2020-11-02 23:55:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((err, req, res, next) => {
 //     console.log('La aplicacion esta corriendo en un localhost:4000!')
 // });
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req,res) => {
   res.sendFile(__dirname + '/index.js')
